5d8a00519e2f35bb980eb9d94fc3fd319f4f9a0d
NataliIvantsova <ivancovanm@yandex.ru>
2023-02-16 00:22:26 +0300

    Задачи 1,2,3

---
+++ HW9_01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в 
+промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"*/
+
+
+void EvenNumbers(int start, int finish)
+{
+    if(start>finish)
+    {
+        return;
+    }
+    if (start%2==0)
+    System.Console.Write(start+" ");
+    EvenNumbers(start+1, finish);
+
+}
+EvenNumbers(1,11);
---
+++ HW9_02/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+void SumOfNatNumbers1(int start, int finish, int sum)
+{
+    if (start > finish)
+    {
+        System.Console.WriteLine($"{sum}");
+        return;
+    }
+    else
+    {
+        sum = sum + ((start)++);
+        SumOfNatNumbers1(start,finish,sum);
+    }
+}
+
+int SumOfNatNumbers2(int start, int finish)
+{
+    if (finish<=start)
+    {
+        return start;
+    }
+    else
+    {
+        //sum = sum + ((start)++);
+        return finish + SumOfNatNumbers2(start,finish-1);
+    }
+}
+
+System.Console.WriteLine("1 способ ");
+SumOfNatNumbers1(1, 15, 0);
+System.Console.WriteLine("2 способ ");
+System.Console.WriteLine(SumOfNatNumbers2(1, 15));
